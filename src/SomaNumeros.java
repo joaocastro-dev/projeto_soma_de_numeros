8880e2f95df6c51f738315e7c0560ffbd1cce9e1
joaocastro-dev <joaocastro12629@gmail.com>
2025-06-03 15:08:01 -0300

    add soma entre numeros

---
+++ src/SomaNumeros.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class SomaNumeros {
+    public static void main(String[] args) {
+        int numero1=10;
+        int numero2=20;
+
+        int soma= numero1+numero2;
+
+        System.out.println("A soma entre os numeros é :"+soma);
+
+    }
+}
